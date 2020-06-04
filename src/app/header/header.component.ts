d71b1c311de100a788d06702a68bc11f8cae3f04
xxBravexx <fegsurvivor@gmx.de>
2020-06-04 15:28:44 +0200

    Directives 3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,13 @@
-import { Component, Output,  EventEmitter} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  collapsed = true;
-
   @Output() featureSelected = new EventEmitter<string>();
 
-  onSelect(feature: string){
+  onSelect(feature: string) {
     this.featureSelected.emit(feature);
   }
 }
